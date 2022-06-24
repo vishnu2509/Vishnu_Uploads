44f8f11c796543a137bfa6c347ddfb70ef7573d7
vishnu2509 <107933278+vishnu2509@users.noreply.github.com>
2022-06-24 19:31:27 +0530

    Add files via upload

---
+++ WebForm2.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebApplication1_ImageUpload_and_Download.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Image ID="Image1" runat="server" />
+    </form>
+</body>
+</html>
---
+++ WebForm2.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1_ImageUpload_and_Download
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBconn"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetImageById", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramId = new SqlParameter()
+                {
+                    ParameterName = "@Id",
+                    Value = Request.QueryString["Id"]
+                };
+                cmd.Parameters.Add(paramId);
+
+                con.Open();
+                byte[] bytes = (byte[])cmd.ExecuteScalar();
+                string strBase64 = Convert.ToBase64String(bytes);
+                Image1.ImageUrl = "data:Image/png;base64," + strBase64;
+            }
+        }
+    }
+}
